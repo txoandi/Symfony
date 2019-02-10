c95e87341cea11e3cb45f8382d30e5d4ea9f18c7
txoandi <txoandi@gmail.com>
2019-02-10 10:30:40 +0100

    Commit inicial

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -5,14 +5,48 @@ use Symfony\Component\Routing\Annotation\Route;
 class DeportesController {
   /**
    * @Route("/deportes")
+
    */
   public function inicio() {
       return new Response('Mi página de deportes!');
   }
   /**
-  * @Route("/deportes/primer-articulo")
-  */
-  public function mostrar() {
-     return new Response('Mi primera ruta Primer Artículo!');
-  }
+* @Route(
+*    "/deportes/{_locale}/{fecha}/{seccion}/{equipo}/{slug}.{_format}",
+*     defaults={"slug": "1","_format":"html"},
+*     requirements={
+*         "_locale": "es|en",
+*         "_format": "html|json|xml",
+*          "fecha": "[\d+]{8}"
+*     }
+* )
+*/
+public function rutaAvanzada($_locale,$fecha, $seccion, $equipo, $slug) {
+ return new Response(sprintf(
+     'Mi noticia en idioma=%s,
+      fecha=%s,deporte=%s,equipo=%s, noticia=%s ',
+     $_locale, $fecha, $seccion, $equipo, $slug));
+}
+
+
+/**
+* @Route(
+*     "/deportes/{_locale}/{fecha}/{seccion}/{equipo}/{pagina}",
+*     defaults={"slug": "1","_format":"html","pagina":"1"},
+*     requirements={
+*         "_locale": "es|en",
+*         "_format": "html|json|xml",
+*         "fecha": "[\d+]{8}",
+*         "pagina"="\d+"
+*     }
+* )
+*/
+public function rutaAvanzadaListado($_locale, $fecha, $seccion, $equipo, $pagina) {
+ return new Response(sprintf(
+     'Listado de noticias  en idioma=%s,
+      fecha=%s,deporte=%s,equipo=%s, página=%s ',
+     $_locale, $fecha, $seccion, $equipo, $pagina));
+}
+
+
 }
